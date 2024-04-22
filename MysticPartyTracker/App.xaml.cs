1802abf527cc077718a88ee2aa9c87c933460188
MukaDeveloper <muka.bertoni@gmail.com>
2024-04-22 19:55:03 -0300

    Initial project

---
+++ MysticPartyTracker/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MysticPartyTracker
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MysticPartyTracker/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MysticPartyTracker
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}

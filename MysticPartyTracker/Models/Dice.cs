e82ae31f2df0d8ea48e551b79cd3773eb37eacfc
MukaDeveloper <muka.bertoni@gmail.com>
2024-04-29 22:48:04 -0300

    patch: initial implementing view model

--- MysticPartyTracker/Models/Dice.cs
+++ MysticPartyTracker/Models/Dice.cs
@@ -3,27 +3,15 @@
     public class Dice
     {
         public int Side { get; set; }
-        public int Quantity { get; set; }
 
-        public Dice(int side, int quantity)
+        public Dice(int side)
         {
-            Quantity = quantity;
             Side = side;
         }
 
         public int Roll()
         {
             return new Random().Next(Side) + 1;
-        }
-
-        public List<int> RollDices()
-        {
-            List<int> dices = new List<int>([]);
-            for (int i = 0; i < Quantity; i++)
-            {
-                dices.Add(Roll());
-            }
-            return dices;
         }
     }
 }
---
+++ MysticPartyTracker/ViewModels/DiceViewModel.cs
@@ -0,0 +1,49 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MysticPartyTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MysticPartyTracker.ViewModels
+{
+    public partial class DiceViewModel : ObservableObject
+    {
+        public DiceViewModel() {
+            RollCommand = new Command(Roll);
+        }
+
+        [ObservableProperty]
+        public int _numberSides;
+
+        [ObservableProperty]
+        public int _quantity;
+
+        [ObservableProperty]
+        public int _total;
+
+        private ObservableCollection<int> _rolls = new ObservableCollection<int>();
+        public ObservableCollection<int> Rolls {
+            get { return _rolls; }
+            set { _rolls = value; } 
+        }
+
+        public ICommand RollCommand { get; }
+
+        public void Roll()
+        {
+            Rolls.Clear();
+            Total = 0;
+            Dice dice = new Dice(NumberSides);
+            for (int i = 0; i < Quantity; i++) //p/ cada nº sorteado, ele sorteia um nº novo
+            {
+                int roll = dice.Roll();
+                Rolls.Add(roll);
+                Total += roll;
+            }
+        }
+    }
+}
